cad74edd99da6f6fab27d9ae945cceca5e170b98
MANEESH SINHA <56319939+KUMAR-MANISH04@users.noreply.github.com>
2023-01-20 22:10:23 +0530

    Time: 0 ms (100.00%), Space: 7.1 MB (99.86%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.cpp
+++ 0412-fizz-buzz/0412-fizz-buzz.cpp
@@ -1,31 +1,60 @@
 class Solution {
 public:
     vector<string> fizzBuzz(int n) {
-        string s;
-        vector<string> ans;
-        for(int i=1;i<=n;i++)
-        {
-            if(i%3==0 && i%5==0)
-            {
-                s="FizzBuzz";
-                ans.push_back(s);
+        vector<string> ans(n);
+
+        for(int i=1;i<=n; i++) {
+            if(i%3==0) {
+                ans[i-1] += "Fizz";
             }
-            else if(i%3==0)
-            {
-                s="Fizz";
-                ans.push_back(s);
+            if(i%5 == 0) {
+                ans[i-1] += "Buzz";
             }
-            else if(i%5==0)
-            {
-                s="Buzz";
-                ans.push_back(s);
-            }
-            else
-            {
-                s=to_string(i);
-                ans.push_back(s);   
+            else if(i%3 !=0) {
+                ans[i-1] = to_string(i);
             }
         }
+
         return ans;
     }
 };
+
+
+
+
+
+
+
+
+
+// class Solution {
+// public:
+//     vector<string> fizzBuzz(int n) {
+//         string s;
+//         vector<string> ans;
+//         for(int i=1;i<=n;i++)
+//         {
+//             if(i%3==0 && i%5==0)
+//             {
+//                 s="FizzBuzz";
+//                 ans.push_back(s);
+//             }
+//             else if(i%3==0)
+//             {
+//                 s="Fizz";
+//                 ans.push_back(s);
+//             }
+//             else if(i%5==0)
+//             {
+//                 s="Buzz";
+//                 ans.push_back(s);
+//             }
+//             else
+//             {
+//                 s=to_string(i);
+//                 ans.push_back(s);   
+//             }
+//         }
+//         return ans;
+//     }
+// };
