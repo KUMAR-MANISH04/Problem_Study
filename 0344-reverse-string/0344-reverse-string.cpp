7bf7f92dab2762454e29d2350d29750bee8aa9a4
MANEESH SINHA <56319939+KUMAR-MANISH04@users.noreply.github.com>
2023-01-21 21:24:23 +0530

    Time: 37 ms (32.28%), Space: 23.2 MB (48.53%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.cpp
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -1,16 +1,41 @@
-class Solution 
-{
-    public:
+class Solution {
+public:
     void reverseString(vector<char>& s) {
-        int start=0;
-        int end=s.size()-1;
-        while(start<end)
-        {
-            char temp=s[start];
-            s[start]=s[end];
-            s[end]=temp;
-            start++;
-            end--;
+        int i=0;int j=s.size()-1;
+        while(i<j){
+            swap(s[i],s[j]);
+            i++;
+            j--;
         }
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Solution 
+// {
+//     public:
+//     void reverseString(vector<char>& s) {
+//         int start=0;
+//         int end=s.size()-1;
+//         while(start<end)
+//         {
+//             char temp=s[start];
+//             s[start]=s[end];
+//             s[end]=temp;
+//             start++;
+//             end--;
+//         }
+//     }
+// };
