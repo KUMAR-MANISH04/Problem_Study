1382728c565bbd2ba1ff716a084348364a8175d6
MANEESH SINHA <56319939+KUMAR-MANISH04@users.noreply.github.com>
2023-01-24 00:47:06 +0530

    Time: 1107 ms (9.74%), Space: 0B (100.00%) - LeetHub

--- 0584-find-customer-referee/0584-find-customer-referee.sql
+++ 0584-find-customer-referee/0584-find-customer-referee.sql
@@ -1,2 +1,6 @@
 SELECT name FROM Customer 
-WHERE id not in (SELECT id FROM Customer WHERE referee_id=2);
+WHERE referee_id != 2 OR referee_id IS NULL;
+
+# SELECT name FROM Customer 
+# WHERE referee_id <> 2 OR referee_id IS NULL;
+# <> is also not equal to sign
