b47d6cfb2e2650ca4b81ec45f7a22b4adf5f54cf
MANEESH SINHA <56319939+KUMAR-MANISH04@users.noreply.github.com>
2023-01-24 00:47:44 +0530

    Time: 875 ms (27.60%), Space: 0B (100.00%) - LeetHub

--- 0584-find-customer-referee/0584-find-customer-referee.sql
+++ 0584-find-customer-referee/0584-find-customer-referee.sql
@@ -1,5 +1,8 @@
-SELECT name FROM Customer 
-WHERE referee_id != 2 OR referee_id IS NULL;
+SELECT name FROM customer 
+WHERE IFNULL(referee_id,0) != 2;
+
+# SELECT name FROM Customer 
+# WHERE referee_id != 2 OR referee_id IS NULL;
 
 # SELECT name FROM Customer 
 # WHERE referee_id <> 2 OR referee_id IS NULL;
