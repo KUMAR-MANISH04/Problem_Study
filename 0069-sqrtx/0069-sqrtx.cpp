88d2761d8b5de69731abf4894ea62789919acd65
MANEESH SINHA <56319939+KUMAR-MANISH04@users.noreply.github.com>
2023-02-22 01:21:44 +0530

    Time: 17 ms (21.79%), Space: 5.9 MB (62.87%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -1,6 +1,39 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        return sqrt(x);
+        long long y=0;
+        while(y*y<=x) y++;
+        return y-1;
+        
     }
 };
+
+
+// class Solution {
+// public:
+//     int mySqrt(int x) {
+//         long long s=0;
+//         long long e=x;
+//         long long ans=0;
+//         while(s<=e){
+//             long long m=(s+e)/2;
+//             if(m*m<=x){
+//                 ans=m;
+//                 s=m+1;
+//             }
+//             else{
+//                 e=m-1;
+//             }
+//         }
+//         return ans;
+//     }
+// };
+
+
+
+// class Solution {
+// public:
+//     int mySqrt(int x) {
+//         return sqrt(x);
+//     }
+// };
