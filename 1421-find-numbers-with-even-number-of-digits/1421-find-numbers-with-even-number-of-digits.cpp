85369766c747ee6dc2d7026e4b30804da237e19d
MANEESH SINHA <56319939+KUMAR-MANISH04@users.noreply.github.com>
2025-04-30 07:41:11 +0530

    Time: 0 ms (100%), Space: 13.4 MB (44.44%) - LeetHub

---
+++ 1421-find-numbers-with-even-number-of-digits/1421-find-numbers-with-even-number-of-digits.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    int findNumbers(vector<int>& nums) {
+        return count_if(nums.begin(), nums.end(), [&](int& x){ 
+            return !((int)log10(x) + 1 & 1); 
+        });
+    }
+};
